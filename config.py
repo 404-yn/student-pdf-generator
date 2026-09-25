13a551532b346238a936c5b8cb2fb4938275f2ec
404-yn <yakob13844698@gmail.com>
2026-09-25 10:02:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OUTPUT_DELETE_AFTER_SECONDS = 10
 OVERLAY = {
     "student_name": {
         "x": 297.64,
-        "y": 737.0,
+        "y": 623.0,
         "font_size": 16,
         "font_name": "Times-Bold",
         "mask_width": 220.0,
@@ -30,7 +30,7 @@ OVERLAY = {
     },
     "register_number": {
         "x": 297.64,
-        "y": 714.0,
+        "y": 600.0,
         "font_size": 15,
         "font_name": "Times-Bold",
         "mask_width": 250.0,
